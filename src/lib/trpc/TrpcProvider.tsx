ee2971993eded6a26755e11276f33dcd6f0acf55
Jozef Bíroš <jozef.biros91@gmail.com>
2024-10-22 17:57:54 +0200

    fix: fixed client's trpcs url for production environment

--- src/lib/trpc/TrpcProvider.tsx
+++ src/lib/trpc/TrpcProvider.tsx
@@ -4,12 +4,25 @@ import { httpBatchLink } from '@trpc/client'
 import { useState, PropsWithChildren } from 'react'
 import trpc from './client'
 
+const getBaseUrl = () => {
+    // browser should use relative path
+    if (typeof window === 'undefined')
+        return ''
+
+    // reference for vercel.com
+    if (process.env.VERCE_URL)
+        return `https://${process.env.VERCEL_URL}`;
+
+    // assume localhost
+    return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 const TrpcProvider = ({ children }: PropsWithChildren) => {
     const [queryClient] = useState(() => new QueryClient({}))
     const [trpcClient] = useState(() => trpc.createClient({
         links: [
             httpBatchLink({
-                url: "http://localhost:3000/api/trpc"
+                url: `${getBaseUrl()}/api/trpc`,
             })
         ]
     }))
