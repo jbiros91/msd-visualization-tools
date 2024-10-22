af3cec411ff59dbc190a7df5151d08ac37f645a9
Jozef Bíroš <jozef.biros91@gmail.com>
2024-10-22 23:45:14 +0200

    feat: implemented simple graph

--- src/app/UKHSATest.tsx
+++ src/app/UKHSATest.tsx
@@ -1,12 +1,20 @@
 'use client'
 import { trpc } from "@/lib/trpc";
+import {useRef} from "react";
+import useChart from "@/app/useChart";
 
 const UKHSATest = () => {
-    const data = trpc.ukhsa.getData.useQuery()
+    const chartContainerRef = useRef<HTMLDivElement>(null);
+    const { chart, setReference } = useChart()
 
+    const { data } = trpc.ukhsa.getData.useQuery()
+
+    console.log(data)
     return (
         <div className="mt-80">
+            <div ref={setReference}></div>
             {JSON.stringify(data)}
+
         </div>
     )
 }
