221863828c3b32395c1d87c0d8cf119e44676255
Jozef Bíroš <jozef.biros91@gmail.com>
2024-10-24 17:53:55 +0200

    chore: suppressed ts error

--- src/common/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/common/components/ErrorBoundary/ErrorBoundary.tsx
@@ -19,7 +19,8 @@ class ErrorBoundary extends Component<PropsWithChildren<Props>, State> {
         hasError: false,
     }
 
-    public static getDerivedStateFromError(_: Error): State {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public static getDerivedStateFromError(error: Error): State {
         // Update state so the next render will show the fallback UI.
         return { hasError: true }
     }
