e251f0d270b929cac3f5141bc6b3376bd32a9946
Jozef Bíroš <jozef.biros91@gmail.com>
2024-10-24 16:30:20 +0200

    fix: fixed condition for env variables

--- src/common/components/TrpcProvider/TrpcProvider.tsx
+++ src/common/components/TrpcProvider/TrpcProvider.tsx
@@ -9,7 +9,7 @@ const getBaseUrl = () => {
     // browser should use relative path
     if (typeof window === 'undefined') return ''
     // reference for vercel.com
-    if (process.env.VERCE_URL) return `https://${process.env.VERCEL_URL}`
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
     // assume localhost
     return `http://localhost:${process.env.PORT ?? 3000}`
 }
--- src/lib/trpcServer.ts
+++ src/lib/trpcServer.ts
@@ -2,11 +2,11 @@ import { httpBatchLink } from '@trpc/client'
 import { createCallerFactory } from '@/server/trpc'
 import { appRouter } from '@/server'
 
-const getBaseUrl = () => {
+export const getBaseUrl = () => {
     // browser should use relative path
     if (typeof window === 'undefined') return ''
     // reference for vercel.com
-    if (process.env.VERCE_URL) return `https://${process.env.VERCEL_URL}`
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
     // assume localhost
     return `http://localhost:${process.env.PORT ?? 3000}`
 }
